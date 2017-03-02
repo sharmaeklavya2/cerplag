2474c2010b734b71331168e03a672068a606b9d4
Eklavya Sharma <ekurgn@gmail.com>
2017-03-02 06:24:30 +0530

    Add bitset

---
+++ util/bitset.h
@@ -0,0 +1,21 @@
+#ifndef H_BITSET
+#define H_BITSET
+
+#include <stdbool.h>
+
+typedef unsigned long long bitset_t;
+typedef int elem_t;
+
+inline static bitset_t bitset_add(bitset_t s, elem_t a)
+{return s | (1ull << a);}
+
+inline static bitset_t bitset_remove(bitset_t s, elem_t a)
+{return s & ~(1ull << a);}
+
+inline static bool bitset_has(bitset_t s, elem_t a)
+{return ((s >> a) & 1ull);}
+
+inline static bitset_t bitset_singleton(elem_t a)
+{return 1ull << a;}
+
+#endif  // H_BITSET
