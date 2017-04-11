3ae979f2df24fd953bbdcd5e5f3612bd69680c26
Eklavya Sharma <ekurgn@gmail.com>
2017-04-12 03:48:33 +0530

    Add header guards to error.h

--- error.h
+++ error.h
@@ -1,3 +1,6 @@
+#ifndef H_ERROR
+#define H_ERROR
+
 extern int error_count;
 extern int warning_count;
 
@@ -5,3 +8,5 @@ enum {ERROR, WARNING};
 
 void print_error(const char* category, int type, int err_num, int line, int col,
     const char* lexeme, const char* err_code, const char* err_msg);
+
+#endif  // H_ERROR
